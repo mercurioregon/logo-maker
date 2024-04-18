f6ee67a2f10969fbcaa0eadcc11e6791a158f7b0
mercurioregon <jamesm620@gmail.com>
2024-04-18 10:35:24 -0700

    README added with video link.  Tests completed.

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# logo-maker
+# Logo Maker
+
+This is an app designed to create a simple logo using prompts in the node command line.  The user may enter text up to 3 characters and choose a text color. The user will also choose the shape of the logo, either triangle, circle, or square. The color of the shape is also chosen.  The completed logo is rendered into an .svg file and is visible in a browser window.
+
+Future improvement of this designs would include sizing options, more shapes to choose from, and more stylistic features with graphics and 3D effects.
+
+Resources:
+
+SVG graphic manipulation-
+
+https://www.w3schools.com/graphics/svg_transformations.asp
+
+Instructional video including tests:
+
+https://drive.google.com/file/d/1bbDWSWMzL0rSFBC4zOV32hc2_7wNcNkj/view
--- test/shapes.test.js
+++ test/shapes.test.js
@@ -6,31 +6,29 @@ const Circle = require("../lib/shapes").Circle
 const {Square} = require("../lib/shapes")
 
 
-// describe("Shapes" , () =>{
+describe("Shapes" , () =>{
 
 describe("Triangle", ()=>{
     it("should be a blue triangle", () => {
         const shape = new Triangle();
-        shape.setColor("blue");
-        expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
+        shape.color("blue");
+        expect(shape.create()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
         })});
 
-describe("Circle", ()=>
-    it("should be a red circle"), ()=> {
+describe("Circle", ()=> {
+    it("should be a red circle", ()=> {
         const shape = new Circle();
-        shape.setColor("red");
-        expect(shape.render()).toEqual('<circle cx="50" cy="50" r= "50"  fill="red" />');
-
+        shape.color("red");
+        expect(shape.create()).toEqual('<circle cx="50" cy="50" r= "50" fill="red" />');
+        });
     });
 
- describe("Square", ()=>
-    it("should be a green square"), ()=> {
+ describe("Square", ()=> {
+    it("should be a green square", ()=> {
         const shape = new Square();
-        shape.setColor("green");
-        expect(shape.render()).toEqual('<rect x="50" y = "50" width ="100" height = "100" fill="green" />');
+        shape.color("green");
+        expect(shape.create()).toEqual('<rect x="50" y = "50" width ="100" height = "100" fill="green" />');
     });
 
-    // });
-
-
-
+    });
+})
